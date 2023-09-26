03eaa2a71837cf4bbe843fd2263d19f84024a9bd
febri <febriroka@gmail.com>
2023-09-26 20:56:38 +0700

    contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	}
+		echo "<h1>"perkenalkan</h1>";
+		echo"nama saya febri roka 
+			 saya tinggal didaerah kaliabang
+			 olahraga yang saya sukai adalah 
+			 bulutangkis";
+	{
+}	
